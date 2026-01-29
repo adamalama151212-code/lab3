4138285915157a85cb4718b7bff11a37af1a1fb6
leszek <adamalama151212@gmail.com>
2026-01-29 22:43:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ window.onload = () => {
             .catch((error) => console.error('Błąd rejestracji:', error));
     }
 };
+
+// Prośba o uprawnienia do powiadomień
+if ("Notification" in window) {
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+            console.log("Uprawnienia do powiadomień przyznane.");
+            // Wyświetlenie testowego powiadomienia po 3 sekundach
+            setTimeout(() => {
+                new Notification("Meme-Koty PWA", {
+                    body: "Zajrzyj do galerii, czekają nowe koty!",
+                    icon: "images/web-app-manifest-192x192.png"
+                });
+            }, 3000);
+        }
+    });
+}
